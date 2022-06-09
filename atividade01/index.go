01e236d2ea2dcf76d79d0ba4cfd4dd3b2daa031e
Victor <joaumvictor.oficial@gmail.com>
2022-06-09 03:55:11 -0300

    first commit

---
+++ atividade01/index.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// isso é um slice ( é tipo um array )
+	ex1 := []string{"nony", "baianor", "bescoito"}
+
+	// isso é um append ( é tipo um .push ou .concat )
+	ex2 := append(ex1, "adran", "diogão")
+
+	// isso aqui é um map ( tipo um objeto )
+	ex3 := map[string]string{"nome": "bescoito"}
+
+	// isso é um console.log humilde
+	fmt.Println(ex1, ex2, ex3)
+}
+
+// pra usar esse tipo de "console.log" o go importa aquele fmt na linha 3
+// todo aplicação tem que ter um package main pra poder rodar
