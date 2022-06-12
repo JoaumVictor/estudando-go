1323915cb292b09871dc01097e6ebdfdc873e875
Victor <joaumvictor.oficial@gmail.com>
2022-06-12 04:30:55 -0300

    separa pastas

--- atividade3/index.go
+++ atividade3/index.go
@@ -2,47 +2,25 @@ package main
 
 import (
 	"fmt"
+	"main/hoofs"
+	"main/models"
+	"main/operations"
 	"sort"
 )
 
-type tipos struct{
-	palavras string
-	numeros int
-	valid bool
-}
-
-func soma(dales ...tipos)(totaln int, totals string){
-	for _, v := range dales{
-		totaln += v.numeros
-		totals += v.palavras
-	}
-	return
-}
-
-
-func find(buscar int, array []int)(result interface{}) {
-	for _, v := range array{
-		if v == buscar {
-			result = v
-		}
-	}
-	return
-}
-
 func main() {
-	molde := make([]tipos, 4)
-
-	nomes := []string{"victor", "alberto", "pasip", "igor"}
-	numeros := []int{4, 2, 3, 1}
-	boolss := []bool{true, false, false, true}
-
+	nomes := []string{"victor", "alberto", "pasip", "igor", "vitinho"}
+	numeros := []int{4, 2, 3, 1, 8}
 	sort.Ints(numeros)
 	sort.Strings(nomes)
 
+	molde := make([]models.Tipos, 0)
 	for i := range nomes{
-		temp := tipos{palavras: nomes[i], numeros: numeros[i], valid: boolss[i]}
-		molde = append(molde, temp)
+		tudao := models.Tipos{Palavras: nomes[i], Numeros: numeros[i]}
+		molde = append(molde, tudao)
 	}
 
-	fmt.Println(find(9, numeros))
+	fmt.Println(molde)
+	fmt.Println(hoofs.Find("victor", nomes))
+	fmt.Println(operations.Soma(numeros))
 }
